6766a0ac5dabb500b223b80c38518a3b29be6bb9
Nicholas Leonard <n.leonard@fi.com>
2023-03-08 07:09:42 -0800

    Very basic console app for testing DocuSign SOAP service calls.

---
+++ src/DocuSignSOAPTest/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FI.ESign.DocuSignSoap;
+using FI.ESign.Common;
+using FI.ESign.Model;
+
+string[] input = Environment.GetCommandLineArgs();
+
+Console.WriteLine($"Using the following arguments: \nUserId: {input[1]}, \npassword: {input[2]}, \nintegratorKey: {input[3]}, \nexternalEnvelopeId: {input[4]}, \nSOAPSvcUrl: {input[5]}");
+Dictionary<string, string> endpoints = new Dictionary<string, string>
+{
+    { "DSSoapSvc", input[5] }
+};
+
+FIDSEnvelopeSvc svc = new FIDSEnvelopeSvc(new FILoginCredential(input[1], input[2], null, input[3], null, null, null, null), endpoints);
+
+var envStatus = svc.GetEnvelopeStatus(new FIEnvelope() { ExternalID = input[4] }, "SOAP Test App");
+
+Console.WriteLine($"EnvelopeStatus: {envStatus.Status}");
